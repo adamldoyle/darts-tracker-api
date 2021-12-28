7800faa9428e2b4823edfcee862f6e6872f1cd54
Adam Doyle <adamldoyle@gmail.com>
2021-12-28 01:22:09 -0600

    Try looking up user

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ const stage = process.env.stage;
 const resourcesStage = process.env.resourcesStage;
 
 const stageConfigs = {
-  dev: {},
-  prod: {},
+  dev: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_RZmCTlmyP',
+  },
+  prod: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_RZmCTlmyP',
+  },
 };
 
 const config = stageConfigs[stage] || stageConfigs.dev;
--- services/leagues-api/leagues.js
+++ services/leagues-api/leagues.js
@@ -1,7 +1,18 @@
 import handler from '../../libs/handler-lib';
+import { region, userPoolId } from '../../config';
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+
+const cognitoClient = new CognitoIdentityServiceProvider({ region });
 
 export const list = handler(async (event, context) => {
-  return event.requestContext;
+  const userSub = event.requestContext.identity.cognitoAuthenticationProvider?.split(':CognitoSignIn:')?.[1] ?? '';
+  const request = {
+    UserPoolId: userPoolId,
+    Filter: `sub = "${userSub}"`,
+    Limit: 1,
+  };
+  const users = await cognitoClient.listUsers(request).promise();
+  return users;
 });
 
 export const get = handler(async (event, context) => {
