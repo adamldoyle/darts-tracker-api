bb5855e3de8ce95b92a99a2206dc974a6e5df30d
Adam Doyle <adamldoyle@gmail.com>
2022-01-01 19:19:40 -0600

    Create league and update membership

--- services/leagues-api/leagues.js
+++ services/leagues-api/leagues.js
@@ -2,8 +2,46 @@ import handler from '../../libs/handler-lib';
 import { getContextAttribute } from '../../libs/auth-lib';
 import dynamoDb from '../../libs/dynamodb-lib';
 
-export const list = handler(async (event, context) => {
+const checkAuth = async (event) => {
   const email = await getContextAttribute(event.requestContext, 'email');
+  if (!email) {
+    throw new Error('Not authenticated');
+  }
+  return email;
+};
+
+const getLeague = (leagueKey, email = undefined) => {
+  const leagueParams = {
+    TableName: 'leagues',
+    Key: {
+      leagueKey: leagueKey,
+    },
+  };
+  const leagueResult = await dynamoDb.get(leagueParams);
+  if (!leagueResult || !leagueResult.Item) {
+    throw new Error('Unknown league');
+  }
+  const league = leagueResult.Item;
+
+  if (email) {
+    const membershipParams = {
+      TableName: 'leaguemembership',
+      Key: {
+        email: email,
+        leagueKey: league.leagueKey,
+      },
+    };
+    const membershipResult = await dynamoDb.get(membershipParams);
+    if (!membershipResult || !membershipResult.Item) {
+      throw new Error('Unknown league');
+    }
+  }
+
+  return league;
+};
+
+export const list = handler(async (event, context) => {
+  const email = await checkAuth(event);
 
   const membershipParams = {
     TableName: 'leaguemembership',
@@ -33,34 +71,19 @@ export const list = handler(async (event, context) => {
 });
 
 export const get = handler(async (event, context) => {
-  const email = await getContextAttribute(event.requestContext, 'email');
-  const leagueKey = event.pathParameters.leagueKey;
+  const email = await checkAuth(event);
 
-  const leagueParams = {
-    TableName: 'leagues',
-    Key: {
-      'leagueKey': leagueKey,
-    },
-  };
-  const leagueResult = await dynamoDb.get(leagueParams);
-  if (!leagueResult) {
-    throw new Error('Unknown league');
-  }
-  const league = leagueResult.Item;
+  const leagueKey = event.pathParameters.leagueKey;
+  const league = getLeague(leagueKey, email);
 
   const membershipParams = {
     TableName: 'leaguemembership',
-    KeyConditionExpression: 'email = :email AND leagueKey = :leagueKey',
+    KeyConditionExpression: 'leagueKey = :leagueKey',
     ExpressionAttributeValues: {
-      ':email': email,
       ':leagueKey': league.leagueKey,
     },
   };
   const membershipResult = await dynamoDb.query(membershipParams);
-
-  if (!membershipResult.Items.find((player) => player.email === email)) {
-    throw new Error('Unknown league');
-  }
 
   return {
     ...league,
@@ -69,9 +92,90 @@ export const get = handler(async (event, context) => {
 });
 
 export const create = handler(async (event, context) => {
+  const email = checkAuth(event);
+
+  const data = JSON.parse(event.body);
+  if (!data.leagueKey || !data.name) {
+    throw new Error(`leagueKey and name required`);
+  }
+  const league = getLeague(data.leagueKey);
+  if (league) {
+    throw new Error(`leagueKey in use`);
+  }
+
+  const leagueParams = {
+    TableName: 'leagues',
+    Item: {
+      leagueKey: data.leagueKey,
+      name: data.name,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  };
+  await dynamoDb.put(leagueParams);
+
+  const membershipParams = {
+    Tablename: 'leaguemembership',
+    Item: {
+      leagueKey: data.leagueKey,
+      email: email,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  };
+  await dynamoDb.put(membershipParams);
+
   return {};
 });
 
 export const patchMembership = handler(async (event, context) => {
+  const email = await checkAuth(event);
+
+  const leagueKey = event.pathParameters.leagueKey;
+  const league = getLeague(leagueKey, email);
+
+  const membershipParams = {
+    TableName: 'leaguemembership',
+    KeyConditionExpression: 'leagueKey = :leagueKey',
+    ExpressionAttributeValues: {
+      ':leagueKey': league.leagueKey,
+    },
+  };
+  const membershipResult = await dynamoDb.query(membershipParams);
+  const membership = membershipResult.Items;
+
+  const data = JSON.parse(event.body);
+  await Promise.all(
+    data
+      .filter((newEmail) => !membership.includes(newEmail))
+      .map((newEmail) => {
+        const membershipParams = {
+          TableName: 'leaguemembership',
+          Item: {
+            leagueKey: league.leagueKey,
+            email: newEmail,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+          },
+        };
+        return dynamoDb.put(membershipParams);
+      }),
+  );
+
+  await Promise.all(
+    membership
+      .filter((oldEmail) => !data.includes(oldEmail) && oldEmail !== email)
+      .map((oldEmail) => {
+        const membershipParams = {
+          TableName: 'leaguemembership',
+          Key: {
+            leagueKey: league.leagueKey,
+            email: oldEmail,
+          },
+        };
+        return dynamoDb.delete(membershipParams);
+      }),
+  );
+
   return {};
 });
